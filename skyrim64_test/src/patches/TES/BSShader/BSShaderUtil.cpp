bf5eba0972920329adf09655d3dc3d09264fdc64
Nukem <Nukem@outlook.com>
2018-01-05 15:47:22 -0500

    GetXMFromNi implemented

--- skyrim64_test/src/patches/TES/BSShader/BSShaderUtil.cpp
+++ skyrim64_test/src/patches/TES/BSShader/BSShaderUtil.cpp
@@ -1,24 +1,44 @@
+#include "../../rendering/common.h"
 #include "../../../common.h"
 #include "BSShaderUtil.h"
 
-DirectX::XMMATRIX BSShaderUtil::GetXMFromNi(const NiTransform& Transform)
-{
-	auto sub_1412DA0D0 = (void(__fastcall *)(DirectX::XMMATRIX *, const NiTransform *))(g_ModuleBase + 0x12DA0D0);
+using namespace DirectX;
 
-	DirectX::XMMATRIX temp;
-	sub_1412DA0D0(&temp, &Transform);
-	return temp;
+XMMATRIX BSShaderUtil::GetXMFromNi(const NiTransform& Transform)
+{
+	return GetXMFromNiPosAdjust(Transform, *(const NiPoint3 *)&GetThreadedGlobals()->__zz2[28]);
 }
 
-DirectX::XMMATRIX BSShaderUtil::GetXMFromNiPosAdjust(const NiTransform& Transform, const NiPoint3& PosAdjust)
+XMMATRIX BSShaderUtil::GetXMFromNiPosAdjust(const NiTransform& Transform, const NiPoint3& PosAdjust)
 {
-	DirectX::XMMATRIX temp = GetXMFromNi(Transform);
-	float *vals = (float *)&temp;
+	XMMATRIX temp;
 
-	vals[12] = Transform.m_Translate.x - PosAdjust.x;
-	vals[13] = Transform.m_Translate.y - PosAdjust.y;
-	vals[14] = Transform.m_Translate.z - PosAdjust.z;
-	vals[15] = 1.0f;
+	const NiMatrix3& m = Transform.m_Rotate;
+	const float scale = Transform.m_fScale;
+
+	temp.r[0] = XMVectorScale(XMVectorSet(
+		m.m_pEntry[0][0],
+		m.m_pEntry[1][0],
+		m.m_pEntry[2][0],
+		0.0f), scale);
+
+	temp.r[1] = XMVectorScale(XMVectorSet(
+		m.m_pEntry[0][1],
+		m.m_pEntry[1][1],
+		m.m_pEntry[2][1],
+		0.0f), scale);
+
+	temp.r[2] = XMVectorScale(XMVectorSet(
+		m.m_pEntry[0][2],
+		m.m_pEntry[1][2],
+		m.m_pEntry[2][2],
+		0.0f), scale);
+
+	temp.r[3] = XMVectorSet(
+		Transform.m_Translate.x - PosAdjust.x,
+		Transform.m_Translate.y - PosAdjust.y,
+		Transform.m_Translate.z - PosAdjust.z,
+		1.0f);
 
 	return temp;
 }
