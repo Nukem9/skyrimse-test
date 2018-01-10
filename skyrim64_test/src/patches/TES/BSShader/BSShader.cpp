71389920f91f1e07276aeb216852e118bcc04eba
Nukem <Nukem@outlook.com>
2018-01-10 03:13:13 -0500

    Fix multithreaded race condition in BSShader::SetBoneMatrix

--- skyrim64_test/src/patches/TES/BSShader/BSShader.cpp
+++ skyrim64_test/src/patches/TES/BSShader/BSShader.cpp
@@ -1,3 +1,4 @@
+#include "../../rendering/common.h"
 #include "../../../common.h"
 #include "BSVertexShader.h"
 #include "BSPixelShader.h"
@@ -41,10 +42,52 @@ void BSShader::ReloadShaders(BSIStream *Stream)
 	sub_141336490(Stream);
 }
 
+AutoPtr(unsigned int, gTlsIndex, 0x34BBA78);
+
 void BSShader::SetBoneMatrix(NiSkinInstance *SkinInstance, Data *Parameters, const NiTransform *Transform)
 {
-	auto sub_1413362C0 = (void(__fastcall *)(BSShader*, NiSkinInstance *SkinInstance, Data *Parameters, const NiTransform *Transform))(g_ModuleBase + 0x13362C0);
-	sub_1413362C0(this, SkinInstance, Parameters, Transform);
+	//auto sub_1413362C0 = (void(__fastcall *)(BSShader*, NiSkinInstance *SkinInstance, Data *Parameters, const NiTransform *Transform))(g_ModuleBase + 0x13362C0);
+	//sub_1413362C0(this, SkinInstance, Parameters, Transform);
+
+	//MemoryContextTracker tracker(26, "BSShader.cpp");
+
+	if (!Parameters || Parameters->m_Flags == 0)
+		return;
+
+	auto *renderer = GetThreadedGlobals();
+
+	uintptr_t v4 = (uintptr_t)SkinInstance;
+	uintptr_t v5 = *(uintptr_t *)(__readgsqword(0x58u) + 8i64 * gTlsIndex);
+
+	if (*(NiSkinInstance **)(v5 + 0x2A00) == SkinInstance)
+		return;
+
+	*(NiSkinInstance **)(v5 + 0x2A00) = SkinInstance;
+
+	auto sub_140D74600 = (void(__fastcall *)(NiSkinInstance *, const NiTransform *))(g_ModuleBase + 0x0D74600);
+	sub_140D74600(SkinInstance, Transform);
+
+	const void *v9 = *(const void **)(v4 + 72);
+	const void *v10 = *(const void **)(v4 + 80);
+	uint32_t v11 = (unsigned int)(3 * *(uint32_t *)(*(uintptr_t *)(v4 + 16) + 88i64));
+
+	{
+		D3D11_MAPPED_SUBRESOURCE map;
+		renderer->m_DeviceContext->Map(renderer->m_ConstantBuffers1[0], 0, D3D11_MAP_WRITE_DISCARD, 0, &map);
+		memcpy_s(map.pData, 16 * v11, v9, 16 * v11);
+
+		renderer->m_DeviceContext->Unmap(renderer->m_ConstantBuffers1[0], 0);
+		renderer->m_DeviceContext->VSSetConstantBuffers(10, 1, &renderer->m_ConstantBuffers1[0]);
+	}
+
+	{
+		D3D11_MAPPED_SUBRESOURCE map;
+		renderer->m_DeviceContext->Map(renderer->m_ConstantBuffers1[1], 0, D3D11_MAP_WRITE_DISCARD, 0, &map);
+		memcpy_s(map.pData, 16 * v11, v10, 16 * v11);
+
+		renderer->m_DeviceContext->Unmap(renderer->m_ConstantBuffers1[1], 0);
+		renderer->m_DeviceContext->VSSetConstantBuffers(9, 1, &renderer->m_ConstantBuffers1[1]);
+	}
 }
 
 bool BSShader::BeginTechnique(uint32_t VertexShaderID, uint32_t PixelShaderID, bool IgnorePixelShader)
