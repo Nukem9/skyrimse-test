8a386e23eff01dd4f24fa39deee8d0106d97ca30
Nukem <Nukem@outlook.com>
2018-01-13 17:52:54 -0500

    ImGui ext: PlotLinesMulti/PlotHistogramMulti

--- skyrim64_test/src/ui/imgui_ext.h
+++ skyrim64_test/src/ui/imgui_ext.h
@@ -4,4 +4,6 @@ namespace ImGui
 {
 	bool BeginGroupSplitter(const char *Header);
 	void EndGroupSplitter();
+	void PlotMultiLines(const char* label, int num_datas, const char** names, const ImColor* colors, float(*getter)(const void* data, int idx), const void * const * datas, int values_count, float scale_min, float scale_max, ImVec2 graph_size);
+	void PlotMultiHistograms(const char* label, int num_hists, const char** names, const ImColor* colors, float(*getter)(const void* data, int idx), const void * const * datas, int values_count, float scale_min, float scale_max, ImVec2 graph_size);
 }
