685ceb1f0e3394005ac11c5b69e72818da146e61
Nukem <Nukem@outlook.com>
2018-10-04 21:01:49 -0400

    CreationKit: Fix when using nsight debugging

--- skyrim64_test/src/d3d9.cpp
+++ skyrim64_test/src/d3d9.cpp
@@ -6,10 +6,9 @@
 typedef long D3DCOLOR;
 typedef void *IDirect3D9;
 
-D3D_EXPORT
-IDirect3D9 *D3DAPI Direct3DCreate9(UINT SDKVersion)
+HMODULE GetD3D9()
 {
-	static auto ptrDirect3DCreate9 = []()
+	static HMODULE hD3D9 = []()
 	{
 		char systemDir[1024];
 		uint32_t len = GetSystemDirectory(systemDir, ARRAYSIZE(systemDir));
@@ -17,7 +16,19 @@ IDirect3D9 *D3DAPI Direct3DCreate9(UINT SDKVersion)
 		Assert(len != 0);
 
 		strcat_s(systemDir, "\\d3d9.dll");
-		return (decltype(&Direct3DCreate9))GetProcAddress(LoadLibraryA(systemDir), "Direct3DCreate9");
+		return LoadLibraryA(systemDir);
+	}();
+
+	// Can't call LoadLibrary multiple times - it might fuck up and return this dll
+	return hD3D9;
+}
+
+D3D_EXPORT
+IDirect3D9 *D3DAPI Direct3DCreate9(UINT SDKVersion)
+{
+	static auto ptrDirect3DCreate9 = []()
+	{
+		return (decltype(&Direct3DCreate9))GetProcAddress(GetD3D9(), "Direct3DCreate9");
 	}();
 
 	return ptrDirect3DCreate9(SDKVersion);
@@ -26,13 +37,23 @@ IDirect3D9 *D3DAPI Direct3DCreate9(UINT SDKVersion)
 D3D_EXPORT
 INT D3DAPI D3DPERF_BeginEvent(D3DCOLOR col, LPCWSTR wszName)
 {
-	return 0;
+	static auto ptrD3DPERF_BeginEvent = []()
+	{
+		return (decltype(&D3DPERF_BeginEvent))GetProcAddress(GetD3D9(), "D3DPERF_BeginEvent");
+	}();
+
+	return ptrD3DPERF_BeginEvent(col, wszName);
 }
 
 D3D_EXPORT
 INT D3DAPI D3DPERF_EndEvent()
 {
-	return 0;
+	static auto ptrD3DPERF_EndEvent = []()
+	{
+		return (decltype(&D3DPERF_EndEvent))GetProcAddress(GetD3D9(), "D3DPERF_EndEvent");
+	}();
+
+	return ptrD3DPERF_EndEvent();
 }
 
 #endif // SKYRIM64_CREATIONKIT_DLL
